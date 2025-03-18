7498d7b63ae2f7491d2c6f23b20abc6ef1968c7c
Michael McGuire <newjoker6@gmail.com>
2025-03-18 09:19:00 -0700

    restructure for assetlib

---
+++ addons/Common Utils/dummyfile.gd
@@ -0,0 +1,15 @@
+@tool
+extends EditorPlugin
+
+# This file does nothing and is required
+# so the assetlib gives you the scripts
+# instead of downloading nothing
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	pass
+
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	pass
